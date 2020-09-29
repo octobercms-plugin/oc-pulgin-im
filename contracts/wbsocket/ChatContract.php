41076abd4426ffcd08527446d77bb15d876061e3
jcc <370234915@qq.com>
2020-09-29 08:29:38 +0800

    可分离到插件

--- contracts/wbsocket/ChatContract.php
+++ contracts/wbsocket/ChatContract.php
@@ -6,18 +6,29 @@ interface ChatContract
 {
 
     public function initData();//初始化信息
-    public function members($data);//群成员列表
-    public function chatRecords($data);//聊天记录
-    public function createGroup($data);//创建群
-    public function applyGroup($data);//申请入群
-    public function agreeGroup($data);//同意入群
-    public function refuseGroup($data);//拒绝入群
-    public function applyFriend($data);//申请好友
-    public function agreeFriend($data);//同意好友
-    public function refuseFriend($data);//拒绝好友
-    public function msgbox($data);//用户的消息盒子
-    public function search($data);//搜索好友或群
 
+    public function members($data);//群成员列表
+
+    public function chatRecords($data);//聊天记录
+
+    //todo 下面的可通过分离到另外一个插件提供
+    public function createGroup($data);//创建群
+
+    public function applyGroup($data);//申请入群
+
+    public function agreeGroup($data);//同意入群
+
+    public function refuseGroup($data);//拒绝入群
+
+    public function applyFriend($data);//申请好友
+
+    public function agreeFriend($data);//同意好友
+
+    public function refuseFriend($data);//拒绝好友
+
+    public function msgbox($data);//用户的消息盒子
+
+    public function search($data);//搜索好友或群
 
 
 }
